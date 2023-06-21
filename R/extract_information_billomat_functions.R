000cfdf51ad3366ef88d8f7da76494566678e110
Leder, Johannes <johannes.leder@uni-bamberg.de>
2023-06-21 12:38:46 +0200

    included whether to check file or not as paramter

--- R/extract_information_billomat_functions.R
+++ R/extract_information_billomat_functions.R
@@ -71,19 +71,93 @@ get_extracted_information <- function(df,field) {
 #' @return the data as df which has id and all information
 
 #' @export
-read_KeysFromDescription <- function(df, sep = ":") {
-
-  df %>%
+read_KeysFromDescription <- function(df, sep = sep,EntryFormatCheck = EntryFormatCheck) {
+  #df <- extracted_information
+  #EntryFormatCheck = FALSE
+  df <- df %>%
     separate(note, into = c("key", "text"), sep = sep) %>%
     drop_na(text) %>%
     mutate(
       text = str_trim(text),
-      text = na_if(text, ""),
-      key = str_replace_all(
-        string = key,
-        pattern = c(".*Leistungszeitraum" = "Leistungszeitraum",
-                    ".* Laufzeit" = "Leistungszeitraum")))
+      text = na_if(text, ""))
 
+  # if the process is not the format check, then I have to correct all wrong entries
+  if (!EntryFormatCheck) {
+
+    replace_string <- c(
+      "–" = "-",
+      "02023" ="2023",
+      "Ende Mai" = "25.5.2022-24.6.2022",
+      "März - Juli 2022" = "1.3.2022-31.7.2022",
+      "Oktober 2021" = "1.10.2021-30.9.2022",
+      "01.06 - 30.11.22" = "01.06.22 - 30.11.22",
+      "14.04.2022" = "14.04.2022-13.10.2022",
+      "10.05.2022\\. \\+ 17.05.2022" = "10.05.2022-17.05.2022",
+      "bis 30.09.2022" = "1.9.2022-30.9.2022",
+      "April 14th, 2022 - October 13th, 2022" = "14.4.2022-13.10.2022",
+      "23.08.2022, 06.09.2022, 13.09.2022, 27.09.2022, 11.10.2022" = "23.08.2022-11.10.2022",
+      "13.07.2021" = "13.07.2021-12.09.2021",
+      #"14.09.2022" = "13.10.2022",
+      "April 14th, 2022" = "14.4.2022",
+      "6 months" = "6Monate",
+      "01.11.2022 - 31.04.2023" = "01.11.2022-30.04.2023",
+      "01.09.2022" = "01.09.2022-31.8.2023",
+      "Anfang Oktober" = "1.10.2022",
+      "27.01, 10.02 , 02.02, 16.02" = "27.01.2023-16.02.2023"
+    )
+
+    to_Leistungsbeginn <- paste0(
+      c(
+        "Versand.*",
+        "Leistungsdatum",
+        "Kampagnenstart",
+        "Campaign launch",
+        "Lieferdatum",
+        "Beginn des Leistungszeitraum",
+        ".osting.*",
+        "Laufzeitbeginn",
+        "Leistungsbeginn.*",
+        "Leistungsstart"
+      ),
+      collapse = "|"
+    )
+
+    to_Leistungsende <- paste0(
+      c(
+        "Kampagnenende",
+        "Laufzeitende",
+        "Ende des Leistungszeitraums"),
+      collapse = "|"
+    )
+
+    to_Leistungszeitraum <- paste0(
+      c(
+        "Laufzeit",
+        "Leistungszeit",
+        "Leistungsbeginn"
+        ),
+      collapse = "|"
+    )
+
+
+    df <- df %>%
+      mutate(key = if_else(str_detect(key, to_Leistungsbeginn) & !str_detect(text, "-"),
+                       "Leistungsbeginn",
+                       if_else(str_detect(key, to_Leistungsende),
+                               "Leistungsende",
+                               if_else(str_detect(key, to_Leistungszeitraum)& str_detect(text, "-|bis|vor|im|nfang"),
+                                       "Leistungszeitraum",
+                                       if_else(str_detect(key,"Laufzeit") & !str_detect(text, "-|bis|vor|im|nfang"),
+                                               "Laufzeit",
+                                               key)
+                                       )
+                               )
+                       ),
+             text = str_replace_all(text, replace_string),
+             text = str_remove_all(text, pattern = c("\\(|\\)|nach erneuter Absprache|&nbsp|-28.02.2023und01.03.2023|-30.06.2023und01.08.2023|- 02.07.2022\\/\\/01.09.2022|-24.06.2022\\(2\\)01.11.2022|-02.07.2022//01.09.2022|nachAbsprache|geplant|EmployerBranding|Stellenanzeigen|1\\.Kampagne|28.02.2023und01.03.2023-|-01.05.2023,01.11.2022|14.11.2023,01.11.2022-"))
+
+      )
+  }
 }
 
 #' extract the information from each note and put it in a long table
@@ -96,36 +170,32 @@ read_KeysFromDescription <- function(df, sep = ":") {
 #' @return the data as df which has id and all information
 
 #' @export
-create_laufzeiten_confirmations <- function(df,confirmation_items,confirmations) {
+create_laufzeiten_confirmations <- function(df,
+                                            confirmation_items,
+                                            confirmations,
+                                            sep = ":",
+                                            EntryFormatCheck = FALSE) {
 
   # keys to keep when filtering
 
   keep_keys <-
     c("Laufzeit",
-      "Kampagnenstart",
-      "Kampagnenende",
-      "Leistungsdatum",
+      "Leistungszeitraum",
       "Leistungsbeginn",
-      "Leistungsende",
-      "Campaign launch",
-      "Running time",
-      "Lieferdatum",
-      "osting am",
-      "Versanddatum"
+      "Leistungsende"
     )%>%
     paste0(.,collapse = "|")
 
   # removing the non laufzeiten rows and coding start and end time
 
   df %>%
-    read_KeysFromDescription(df = .) %>%
+    read_KeysFromDescription(df = ., sep = sep, EntryFormatCheck = EntryFormatCheck) %>%
     left_join(confirmation_items %>%
                 select(id,title,confirmation_id,article_id), by = "id") %>%
     left_join(confirmations %>%
                 select(id,client_id,date,confirmation_number), by = c("confirmation_id"="id")) %>%
     group_by(key,id) %>%
-    mutate(Number = n()) %>%
-    filter(str_detect(key, pattern = keep_keys)) %>%
+    mutate(Number = row_number()) %>%
     mutate(Produkt = if_else(
       !str_detect(title, "Bewerber-Boost"),
       title,
@@ -139,19 +209,15 @@ create_laufzeiten_confirmations <- function(df,confirmation_items,confirmations)
           paste0(title, "_Social_Media_Post")
         )
       )
-    ),
-    key = if_else(str_detect(key, "Versand"),"Versanddatum",
-                  if_else(str_detect(key, "Laufzeit"),"Laufzeit",
-                          key)
     )
     ) %>%
+    filter(str_detect(key, pattern = keep_keys)&!str_detect(key, pattern = "Das Karriereprofil|Stellen")) %>%
     pivot_wider(names_from = key, values_from = text) %>%
+    separate(Leistungszeitraum,into = c("Start","Ende"), sep = "-") %>%
     mutate(
-      Leistungsbeginn = as.Date(
-        coalesce(Leistungsbeginn, Versanddatum, Leistungsdatum),
-        format = "%d.%m.%Y"
+      Leistungsbeginn = as.Date(coalesce(Leistungsbeginn,Start), format = "%d.%m.%Y"
       ),
-      Leistungsende = as.Date(coalesce(Leistungsende),
+      Leistungsende = as.Date(coalesce(Leistungsende,Ende),
                               format = "%d.%m.%Y"
       )
     )
