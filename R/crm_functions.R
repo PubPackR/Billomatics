442dbb35755cf6e6462294e3f525e458b5e9b860
Moritz Hemmann <morhe00@gmail.com>
2024-01-18 16:58:23 +0100

    added download of attachments

--- R/crm_functions.R
+++ R/crm_functions.R
@@ -224,7 +224,7 @@ get_central_station_cal_events <- function (api_key, pages = 1000) {
 
 #' @param api_key the api key you have to provide
 #' @param filter_by setting to prefilter export by a list of "company_id" or "person_id", if all protocols are needed use FALSE
-#' @param filter_vector vector to filter by "company_id" or "person_id" (decided by filter_by), not needed if all protocolls should get downloaded
+#' @param filter_vector vector to filter by "company_id" or "person_id" (decided by filter_by), not needed if all protocols should get downloaded
 #' @param pages the setting to set a maximum number of pages to download, with 2000 pages as default (one page includes 250 entries)
 #' @return the tibble which contains all information, this can be stored in a single vector or lists
 
@@ -317,6 +317,60 @@ get_central_station_protocols <- function (api_key, filter_by = FALSE, filter_ve
   }
 
   protocols %>% tidyr::unnest(protocol_object_note)
+}
+
+#' get_central_station_protocols
+#'
+#' this function calls the crm api and downloads the data of attachments for a 
+#' given vector of protocol-ids
+
+#' @param api_key the api key you have to provide
+#' @param protocols_vector vector with protocols, where we want to export the attachments from
+#' @return the tibble which contains all information, this can be stored in a single vector or lists
+
+#' @export
+get_central_station_attachments <- function (api_key, protocols_vector) {
+
+  # define header
+  headers <- c(
+    "content-type" = "application/json",
+    "X-apikey" = crm_api_key,
+    "Accept" = "*/*"
+  )
+  
+  # create an data.frame for attachments
+  attachments <- tidyr::tibble()
+
+  for (i in 1:length(protocols_vector)) {
+    # create an response with httr and the GET function. In the function you paste
+    # url and your endpoint and you also need the headers
+    tryCatch({
+      response <-
+        httr::GET(
+          # set url with "page=" so you can add endpoints and defined filter
+          paste0(
+            "https://api.centralstationcrm.net/api/protocols/",
+            protocols_vector[i],
+            "/attachments?perpage=250&page=1"
+          ),
+          httr::add_headers(headers)
+        )
+      
+      data <- jsonlite::fromJSON(httr::content(response, "text")) %>%
+        select(-data)
+      attachments <- dplyr::bind_rows(attachments, data)
+    },
+    error = function(cond) {
+      message("Attachments do not exist")
+      # Choose a return value in case of error
+      NA
+    })
+    if (i %% 10 == 0) {
+      print(i)
+    }
+  }
+  
+  return(attachments)
 }
 
 
