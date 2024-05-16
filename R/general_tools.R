d85797a1df2fe7ca188363f5c14f1e6bb36b8124
Moritz Hemmann <morhe00@gmail.com>
2024-05-16 12:58:20 +0200

    add function to generate downloadable excel and update passwort-file

--- R/general_tools.R
+++ R/general_tools.R
@@ -36,3 +36,34 @@ generate_password <- function(length = 10) {
   # Return the password as a single string
   return(paste(password, collapse = ""))
 }
+
+#' save_downloadable_excel
+#'
+#' This function generates a random password, creates a downloadable Excel file
+#' with the provided filename, and saves it in the specified directory. It also
+#' manages password encryption and updates the password log.
+
+#' @param dashboard_nr the dashboard number to be included in the file path
+#' @param file_name the name of the Excel file to be generated
+#' @return no return values
+
+#' @export
+save_downloadable_excel <- function(data, dashboard_nr, file_name) {
+
+  # Generate a random password for Excel file encryption
+  pwd_excel <- generate_password(12)
+
+  # Create the Excel file with KPI data and password protection
+  data %>%
+    write.xlsx(paste0(shiny_download_files, "/shiny_", dashboard_nr, "/", file_name), password = pwd_excel)
+
+  # Load and update the password log, encrypting it for security
+  passwort_df <- readRDS(paste0(shiny_download_files, "/encryption_download_files.RDS")) %>%
+    safer::decrypt_object(keys$billomat[1]) %>%
+    bind_rows(data.frame(file = paste0("shiny_", dashboard_nr, "/", file_name), password = pwd_excel, ts = Sys.time())) %>%
+    arrange(ts) %>%
+    group_by(file) %>%
+    summarise_all(last) %>%
+    safer::encrypt_object(keys$billomat[1]) %>%
+    saveRDS(paste0(shiny_download_files, "/encryption_download_files.RDS"))
+}
