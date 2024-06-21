16ffb22a87aaee12bd2be95fd0ad35969d68b55a
LenaKuester <lena.kuester@studyflix.de>
2024-06-21 09:52:29 +0200

    added function put_invoice_value

--- R/check_balance.R
+++ R/check_balance.R
@@ -17,3 +17,47 @@ set_label_invoice <- function(invoices_checked_df,
            is_checked = id %in% invoices_checked_df$id)
 
 }
+
+
+#' put_invoice_value
+#'
+#' This function puts a value in a selected field via Billomat API for each invoice in a df
+#'
+#' @param billomat_api_key The Billomat API key
+#' @param invoice_draft_df The df containing the invoices
+#' @param field The field (e.g., "label", "intro", etc.) that should be put to Billomat
+#'
+#' @export
+put_invoice_value <- function(billomat_api_key, invoice_draft_df, field) {
+
+  billomat_id <- "k16917022"
+
+  header <- c("X-BillomatApiKey" = billomat_api_key,
+              "Content-Type" = "application/json")
+
+  entity_id <- invoice_draft_df$id
+
+  field_text <- invoice_draft_df[[field]]
+
+  for (i in 1:nrow(invoice_draft_df)) {
+
+    api_endpoint <- paste0("https://", billomat_id, ".billomat.net/api/invoices/", entity_id[i])
+
+    body_values <- list()
+    body_values[["invoices"]][[field]] <- field_text[i]
+
+    response <- httr::PUT(url = api_endpoint,
+                          httr::add_headers(header),
+                          body = jsonlite::toJSON(body_values),
+                          httr::accept("application/json")
+    )
+
+    content <- httr::content(response)
+
+    print(paste("Der Statuscode:", response[["status_code"]]))
+
+    if(response[["status_code"]] != 200){
+      print(content[["errors"]])
+    }
+  }
+}
